32d1e8728301716860ff1101ac34539e71f48d8d
Taylor Smith <twsmith89@gmail.com>
2018-09-26 07:56:57 +0000

    add layout to blog posts

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import Layout from '../components/layout'
 
 export default function Template({
     data
@@ -7,15 +8,18 @@ export default function Template({
     const { markdownRemark } = data
     const { frontmatter, html } = markdownRemark
     return (
-        <div className="blog-post-container">
-            <div className="blog-post">
-                <h1>{frontmatter.title}</h1>
-                <h2>{frontmatter.date}</h2>
-                <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }}>
-                
+        <Layout>
+            <div className="blog-post-container">
+                <div className="blog-post">
+                    <h1>{frontmatter.title}</h1>
+                    <h2>{frontmatter.date}</h2>
+                    <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }}>
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </Layout>
+
     )
 }
 
